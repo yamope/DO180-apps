dab6f33ac0ff3e39f91771ada130399be0ffd926
Student User <student@workstation.lab.example.com>
2022-11-08 05:02:11 -0500

    change

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Iam here , the thing you cannot compleate today alway can clear tomorrow , persitert. "\n";
+print "Iam here , the thing you cannot compleate today alway can clear tomorrow , persitert. \n";
 ?>
